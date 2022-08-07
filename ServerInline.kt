6e910c0c36b720f6f32e322047b0ecaa7c8ba2a0
Alexey Eremin <a.eremin.msu@gmail.com>
2022-08-07 13:17:34 +0200

    Super mimimalistic example

--- ServerInline.kt
+++ ServerInline.kt
@@ -1,25 +1,13 @@
 package com.example
 
-import io.ktor.server.application.Application
-import io.ktor.server.application.log
-import io.ktor.server.application.call
 import io.ktor.server.engine.*
-import io.ktor.server.netty.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
 
+//
 fun main() {
     val environment = applicationEngineEnvironment {
-        module {
-            main()
-        }
 
-        connector {
-            port = 8080
-            host = "127.0.0.1"
-        }
     }
-    Application(environment)
+    // Application(environment)
 }
 
 
